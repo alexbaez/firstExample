a1d855a5822123a7768435fedeb766873ee83034
alexbaez <Alexander.Baez@quinnipiac.edu>
2015-10-13 21:06:36 -0400

    i added an add function

---
+++ Arithmetic.java
@@ -0,0 +1,11 @@
+public class Arithmetic{
+	public static void main(String[] args){
+		System.out.println("hellow world"):
+	
+		system.out.println(add(10,4));
+	}
+
+	public static int add(int a, int b){
+		return a+b;	}
+
+}
